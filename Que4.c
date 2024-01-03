9db6fbc2d8921e9454b52b8083c4e74afdbb11cb
Jemish88 <jemish.khunt88@gmail.com>
2024-01-03 19:03:48 +0530

    practical

--- Que4.c
+++ Que4.c
@@ -2,57 +2,28 @@
 Q.4 WAP to print all even numbers from a given 2D array using Pointer.
 */
 void main(){
-	int i,j,n,m,p;
+	int n,i,sum=0;
 	
-	printf("Enter row size:");
+	printf("Enter size of array:");
 	scanf("%d",&n);
 	
-	printf("Enter column size:");
-	scanf("%d",&m);
+	int a[n];
+	int *ptr[n];
 	
-	int a[i][j],b[i][j];
-	
-	printf("Enter value of a:-");
 	for(i=0;i<n;i++)
 	{
-		for(j=0;j<=m;j++)
-		{
-		   printf("Enter a[%d][%d]:",i,j);
-		   scanf("%d",&a[i][j]);
-		}
+	  printf("Enter a[%d]",i);
+      scanf("%d",&a[i]);
+      ptr[i]=&a[i];
 	}
 	
-	printf("Enter value of b:-");
+	printf("Even numbers from array\n:");
 	for(i=0;i<n;i++){
-		for(j=0;j<=m;j++){
-			printf("Enter b[%d][%d]:",i,j);
-		scanf("%d",&b[i][j]);
-		}
+		if(*ptr[i]%2==0){
+			printf("%d\n",*ptr[i]);
+			sum=sum+*ptr[i];
+		}	
 	}
-	
-	printf(" even numbers are: ");
-	printf("\n");
-	
-	p=&a[i][j]+b[i][j];
-	
-     for(i=0;i<n;i++){
-     	for(j=0;j<m;j++){
-     		if(p%2==0){
-     			printf("%d",p);
-			 }
-		 }
-	 }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	 printf("sum of even numbers is % d",sum);
 	
 }
--- que3.c
+++ que3.c
@@ -1,23 +1,39 @@
 /*
 Q.3 WAP to find Fibonacci Series to n Numbers and Store it into textfile. 
+
 */
-void main(){
-	int n1=0,n2=1,n3,n;
-	
-	printf("Enter any number:-");
-	scanf("%d",&n);
-	
-	printf("%d,%d;:n1,n2");
-	
-	n3=n%10;
-	
-	while(n)
-	{
-	 n=n/10;
-	 printf("%d",n);
-	 n++;
-	 n1=n2;
-	 n2=n3;
-	}
-	
+#include<stdio.h>
+void f(int n, FILE *file) {
+	int i;
+    int a = 0, b = 1, c;
+
+    fprintf(file, "%d ", a);
+    fprintf(file, "%d ", b);
+ 
+    for( i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+        fprintf(file, "%d ", c);
+    }
+}
+
+int main() {
+    int n;
+    FILE *file;
+
+    printf("Enter the elements:  ");
+    scanf("%d", &n);
+
+    file = fopen("fibonacci.txt", "w");
+    if(file == NULL) {
+        printf("file could not open\n");
+    }
+
+    f(n, file);
+
+
+    printf("Fibonacci series is written in 'fibonacci.txt'.\n");
+
+   
 }
